e2fd6a1cd12d2588faff1c33eb30747f8c1a7a33
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2021-02-11 14:05:51 +0100

    Create skeleton, implement environment selection

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zeit.nightwatch',
+    version='1.0.0.dev0',
+    author='Zeit Online Backend',
+    author_email='zon-backend@zeit.de',
+    description='',
+    long_description=(
+        open('README.rst').read() +
+        '\n\n' +
+        open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='GPL v2',
+    install_requires=[
+        'pytest',
+        'setuptools',
+    ],
+    entry_points={
+        'pytest11': ['zeit_nightwatch=zeit.nightwatch.pytest'],
+    }
+)
---
+++ src/zeit/nightwatch/pytest.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--nightwatch-environment', default='staging',
+        help=nightwatch_environment.__doc__)
+
+
+@pytest.fixture(scope='session')
+def nightwatch_environment(request):  # convenience spelling
+    """Run tests against this environment (staging, production, etc.)"""
+    return request.config.getoption('--nightwatch-environment')
+
+
+@pytest.fixture(scope='session')
+def zeitde(nightwatch_environment):
+    if nightwatch_environment == 'production':
+        return lambda x: 'https://%s.zeit.de' % x
+    else:
+        return lambda x: 'https://%s.%s.zeit.de' % (x, nightwatch_environment)
