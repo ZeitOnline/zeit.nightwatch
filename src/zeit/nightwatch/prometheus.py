5db3836519af018863fd90fb7e6e24e78f55beef
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2021-02-12 11:05:59 +0100

    Include prometheus functionality directly
    - Wholesale copy&paste of https://github.com/yuvipanda/pytest-prometheus/blob/ca8611c/pytest_prometheus/__init__.py (which is only about half a page of code anyway)
    - Fix pushgateway bug described in https://github.com/yuvipanda/pytest-prometheus/pull/2
      by collecting metrics and sending them off once at the session end
    - Support sending the test name as a label
      (See also https://github.com/yuvipanda/pytest-prometheus/issues/3)

---
+++ src/zeit/nightwatch/prometheus.py
@@ -0,0 +1,64 @@
+import prometheus_client
+
+
+def addoption(parser):
+    group = parser.getgroup('terminal reporting')
+    group.addoption(
+        '--prometheus', action='store_true', default=False,
+        help='Send metrics to prometheus')
+    group.addoption(
+        '--prometheus-pushgateway-url',
+        default='https://prometheus-pushgw.ops.zeit.de',
+        help='Push Gateway URL to send metrics to')
+    group.addoption(
+        '--prometheus-metric-name', default='nightwatch_check',
+        help='Name for prometheus metrics, can contain {funcname} placeholder')
+    group.addoption(
+        '--prometheus-extra-labels', action='append',
+        help='Extra labels to attach to reported metrics')
+    group.addoption(
+        '--prometheus-job-name', default='unknown',
+        help='Value for the "job" key in exported metrics')
+
+
+def configure(config):
+    if not config.getoption('--prometheus'):
+        return
+
+    if config.option.prometheus_extra_labels is None:
+        config.option.prometheus_extra_labels = []
+    config.option.prometheus_extra_labels.append(
+        'environment=%s' % config.getoption('--nightwatch-environment'))
+
+    config._prometheus = PrometheusReport(config)
+    config.pluginmanager.register(config._prometheus)
+
+
+def unconfigure(config):
+    if getattr(config, '_prometheus', None) is not None:
+        config.pluginmanager.unregister(config._prometheus)
+        del config._prometheus
+
+
+class PrometheusReport:
+
+    def __init__(self, config):
+        self.config = config
+        self.registry = prometheus_client.CollectorRegistry()
+
+    def pytest_runtest_logreport(self, report):
+        if report.when != 'call':
+            return
+        opt = self.config.option
+        labels = dict(x.split('=') for x in opt.prometheus_extra_labels)
+        labels['test'] = report.location[2]
+        metric = prometheus_client.Gauge(
+            opt.prometheus_metric_name.format(funcname=report.location[2]), '',
+            labels.keys(), registry=self.registry)
+        metric.labels(**labels).set(1 if report.outcome == 'passed' else 0)
+
+    def pytest_sessionfinish(self, session):
+        opt = self.config.option
+        prometheus_client.push_to_gateway(
+            opt.prometheus_pushgateway_url, job=opt.prometheus_job_name,
+            registry=self.registry)
