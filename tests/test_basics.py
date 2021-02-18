f0b9a1796d3b75aba30a45d3a22a0f8578eab145
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2021-02-18 11:55:51 +0100

    Write basic selenium test

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -3,9 +3,15 @@ import pytest
 
 @pytest.fixture(scope='session')
 def nightwatch_config():
-    return dict(browser=dict(baseurl='https://httpbin.org'))
+    return dict(browser=dict(baseurl='https://httpbin.org'),
+                selenium=dict(baseurl='https://httpbin.org'))
 
 
 def test_get(http):
     r = http.get('/get')
     assert r.status_code == 200
+
+
+def test_selenium_works(selenium):
+    selenium.get('/forms/post')
+    assert selenium.find_element_by_css_selector('form')
