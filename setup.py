8284f02134652cf46922efe9d984715dc75c9228
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-12-12 15:43:20 +0100

    Back to development: 1.9.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="zeit.nightwatch",
-    version="1.9.2",
+    version="1.9.3.dev0",
     author="Zeit Online",
     author_email="zon-backend@zeit.de",
     url="https://github.com/ZeitOnline/zeit.nightwatch",
