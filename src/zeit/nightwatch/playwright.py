8cfbc38611ed7e4510c51e6856b8e6e09a237c2a
Tobias Kabbeck <tobias.kabbeck@gmail.com>
2025-04-03 14:46:43 +0200

    test(nightwatch): assert redirected page after login form submit The default redirect to /konto page has already been solved by https://github.com/ZeitOnline/zeit.nightwatch/pull/18 while working on this. This PR brings addiotinally the redirect handling for old meine.zeit `url` and new login.zeit `redirect_uri` Refs: ENG-160

--- src/zeit/nightwatch/playwright.py
+++ src/zeit/nightwatch/playwright.py
@@ -1,8 +1,14 @@
+from urllib.parse import parse_qs, urljoin, urlparse
+
 from playwright.sync_api._generated import Page
 
 
 def sso_login(self, url, username, password):
     self.goto(url)
+    query_params = parse_qs(urlparse(url).query)
+    redirect_url_list = query_params.get("url")
+    redirect_uri_list = query_params.get("redirect_uri")
+
     if self.locator("#kc-login").count() == 1:
         self.locator("#username").fill(username)
         self.locator("#password").fill(password)
@@ -11,7 +17,14 @@ def sso_login(self, url, username, password):
         self.locator("#login_email").fill(username)
         self.locator("#login_pass").fill(password)
         self.locator("input.submit-button.log").click()
-    self.wait_for_url("**/konto")
+
+    if redirect_url_list or redirect_uri_list:
+        redirect_list = redirect_url_list or redirect_uri_list
+        redirect = redirect_list.pop()
+        target = urljoin(redirect, urlparse(redirect).path)
+        self.wait_for_url(target + "**")
+    else:
+        self.wait_for_url("**/konto")
 
 
 Page.sso_login = sso_login
